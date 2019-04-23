da94802179a3762ec9f02ced25e8581710c372c6
Hardik Gondaliya <hardikg23@gmail.com>
2019-04-23 18:17:47 +0530

    config fix

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const MasterKeys = {
   development: {
-    apiOauthEndPoint: 'http://localhost:3000/oauth/token',
+    apiOauthEndPoint: 'http://localhost:3000/oauth/token.json',
     apiEndPoint: 'http://localhost:3000/api/v3/',
     clientId: '94755f4859da968586e894f5f699c98efd77d4a9737a9f74faad0fac04210814',
     facebookAppId: '669362659769837',
     googleClientId: '264550764233-aqbbl4las10s5avh3tbg2cedkr0ahavp.apps.googleusercontent.com'
   },
   production: {
-    apiOauthEndPoint: 'https://www.fanspole.com/oauth/token',
+    apiOauthEndPoint: 'https://www.fanspole.com/oauth/token.json',
     apiEndPoint: 'https://www.fanspole.com/api/v3/',
     clientId: '94755f4859da968586e894f5f699c98efd77d4a9737a9f74faad0fac04210814',
     facebookAppId: '1396301354009463',
